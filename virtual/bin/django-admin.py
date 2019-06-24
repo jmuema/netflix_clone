0a68be9651d3e59182c40b6b38f7f445bfc80618
josephmuema <jmuema95@gmail.com>
2019-06-24 18:24:48 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/joseph/Documents/side-projectz/netflix-clone254/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
