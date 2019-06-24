d79fec1cf47b044cf3eb79e2c4b78c857601a5a6
josephmuema <jmuema95@gmail.com>
2019-06-24 19:05:44 +0300

    created a file for my html files

--- movies/urls.py
+++ movies/urls.py
@@ -7,3 +7,6 @@ urlpatterns=[
  url(r'^$', views.movies, name='myMovies'),
  url(r'^movie/(\d+)', views.single_movie, name = 'oneMovie')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
