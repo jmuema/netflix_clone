1f9209e4c3e614c6170f812840aba9708c95fa64
josephmuema <jmuema95@gmail.com>
2019-06-24 18:58:37 +0300

    create a function for movies

--- movies/views.py
+++ movies/views.py
@@ -17,3 +17,13 @@ YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
 # Create your views here.
+
+def movies(request):
+    popular_movies_tmdb = tmdb.Movies('popular')
+    popular_movies = popular_movies_tmdb.info()['results']
+
+    upcoming_movies_tmdb = tmdb.Movies('upcoming')
+    upcoming_movies = upcoming_movies_tmdb.info()['results']
+
+    return render(request, 'movies.html', {'popular':popular_movies, 'upcoming':upcoming_movies})
+
