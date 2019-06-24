2ece09264eacda1ee4ff9247528a3d9e920ff83d
josephmuema <jmuema95@gmail.com>
2019-06-24 18:40:48 +0300

    imported a few modules

--- movies/views.py
+++ movies/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import tmdbsimple as tmdb
+import json
+from time import mktime
+import time
+from datetime import datetime, date
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 # Create your views here.
