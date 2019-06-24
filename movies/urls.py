e66a00da512bc55b956d1c193ea8fd4af3b32ee4
josephmuema <jmuema95@gmail.com>
2019-06-24 19:02:35 +0300

    created the various url paths

--- movies/urls.py
+++ movies/urls.py
@@ -2,3 +2,8 @@ from django.conf.urls import url
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns=[
+ url(r'^$', views.movies, name='myMovies'),
+ url(r'^movie/(\d+)', views.single_movie, name = 'oneMovie')
+]
